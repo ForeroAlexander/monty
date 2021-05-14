fc30e1dd73395102cf23f6bd5dd70d0b739e1929
ForeroAlexander <foreroalex26@gmail.com>
2021-05-13 20:12:19 -0500

    fixx2

--- util_func.c
+++ util_func.c
@@ -1,15 +1,4 @@
 #include "monty.h"
-/**
- * error_exit - frees the stack and exits due to erro
- * @stack: pointer to the head of the stack
- *
- */
-void error_exit(stack_t **stack)
-{
-	if (*stack)
-		free_dlistint(*stack);
-	exit(EXIT_FAILURE);
-}
 
 /**
  * _rotl - function that rotates the stack to the top.
