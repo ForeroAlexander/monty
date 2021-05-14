d337204e3fb21d1f9728ecb6794fa648350d10e0
ForeroAlexander <foreroalex26@gmail.com>
2021-05-13 22:30:55 -0500

    si no funciona me pego un triro con un banano

--- get_div_line.c
+++ get_div_line.c
@@ -1,28 +1,29 @@
 #define GLOBALS
 #include "monty.h"
 
+
 /**
- * get_div_line - functions that tokenize the line
+ * get_div_line - function that tokenizes the line
  * @line: is a character
  * Return: arrays of strings
  */
 char **get_div_line(char *line)
 {
-    char **divide;
-    int i = 0;
+	char **divide;
+	int i = 0;
 
-    divide = malloc(1024 * sizeof(char *));
-    divide[i] = strtok(line, " \n\t");
-    if (divide[i] == NULL)
-    {
-        free(divide);
-        return (NULL);
-    }
-    while (divide[i])
-    {
-    i++;
-    divide[i = strtok(NULL, " \n\t");   
-    }
-    divide[i + 1] = NULL;
-    return (divide);
+	divide = malloc(1024 * sizeof(char *));
+	divide[i] = strtok(line, " \n\t");
+	if (divide[i] == NULL)
+	{
+		free(divide);
+		return (NULL);
+	}
+	while (divide[i])
+	{
+		i++;
+		divide[i] = strtok(NULL, " \n\t");
+	}
+	divide[i + 1] = NULL;
+	return (divide);
 }
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 /**
  * select_instruction - function that sort the opcodes
- * 
+ *
  * Return: Void function
  */
 void select_instruction(void)
@@ -17,14 +17,14 @@ void select_instruction(void)
 	gbl.line_number = 1;
 	while (getline(&(gbl.line), &l_sz, gbl.bt_code) != -1)
 	{
-		gbl.div_line = get_div_line(gbl.line)
+		gbl.div_line = get_div_line(gbl.line);
 		if (gbl.div_line == NULL)
 		{
 			gbl.line_number++;
 			continue;
 		}
 		gbl.num = gbl.div_line[1];
-		monty_func(&stack);
+		exe_function(&stack);
 		if (strcmp(gbl.div_line[0], "stack") == 0)
 			gbl.mode = 1;
 		else if (strcmp(gbl.div_line[0], "queue") == 0)
@@ -36,25 +36,25 @@ void select_instruction(void)
 	free_dlistint(stack);
 }
 /**
- * main - driver function for monty program
- * @ac: int num of arguments
- * @av: opcode file
- * Return: 0
+ * main - function that calls the opcodes
+ * @ac: is an integer
+ * @av: is a character
+ * Return: Always 0
  */
 int main(int ac, char **av)
 {
-		if (ac != 2)
-		{
-			dprintf(STDERR_FILENO, "USAGE: monty file\n");
-			exit(EXIT_FAILURE);
-		}
-		gbl.bt_code = fopen(av[1], "r");
-		if (gbl-bt_code == NULL)
-		{
-			dprintf(STDERR_FILENO, "Error: can't open file %s\n", av[1]);
-			exit(EXIT_FAILURE);
-		}
-		select_instruction();
-		fclose(gbl.bt_code)
-		return (0);
+	if (ac != 2)
+	{
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	gbl.bt_code = fopen(av[1], "r");
+	if (gbl.bt_code == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+	select_instruction();
+	fclose(gbl.bt_code);
+	return (0);
 }
